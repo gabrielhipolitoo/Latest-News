1abc9879ee9d3b8174011f26d60df0cd76165a2d
gabrielhipolitoo <c.gabrielhipolitoh@gmail.com>
2024-07-07 13:19:23 -0300

    save to use in other pc

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import express from "express";
 import path from "path";
+import { engine } from "express-handlebars";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 class Server {
   constructor() {
     const { app, port } = this.configServer();
     this.startServer(app, port);
+    this.routes(app);
   }
 
   configServer() {
     const app = express();
     const port = 3000;
-    const __dirname = path.dirname;
     app.use(express.json());
+    app.use(
+      express.urlencoded({
+        extended: true,
+      }),
+    );
     app.use(express.static("public"));
-    console.log("ola");
+    app.set("view engine", "handlebars");
+    app.engine("handlebars", engine());
+    app.set("views", path.join(__dirname, "public/views"));
+
     return {
       app,
       port,
@@ -26,6 +39,21 @@ class Server {
     return {
       //null
     };
+  }
+
+  routes(app) {
+    app.get("/", (req, res) => {
+      res.render("index", { lengthSubscribe: 2 });
+    });
+
+    app.post("/subscribe", (req, res) => {
+      const { email } = req.body;
+      res.redirect("/subscribe");
+    });
+
+    app.get("/subscribe", (req, res) => {
+      res.render("subscribe");
+    });
   }
 }
 
